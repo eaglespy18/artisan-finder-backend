eedb60f59a25b67f14a09feae850ca8bc22bd0a1
mo <alhassankhidir@gmail.com>
2025-09-25 14:32:33 -0700

    Connected backend with frontend and tested artisans fetch

---
+++ middleware/handleValidation.js
@@ -0,0 +1,16 @@
+// middleware/handleValidation.js
+const { validationResult } = require("express-validator");
+
+exports.handleValidation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array().map(err => ({
+        field: err.path,
+        message: err.msg
+      }))
+    });
+  }
+  next();
+};
